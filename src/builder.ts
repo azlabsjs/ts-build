3c9dcf1d711e9a1f0ee6a62d24280e29ffdcbeb0
azandrew-sidoine <azandrewdevelopper@gmail.com>
2024-05-16 19:30:37 +0000

    Fixed bug in builder.ts:43 releated to copying typescript definition files

--- src/builder.ts
+++ src/builder.ts
@@ -40,7 +40,7 @@ export async function createBuildConfigs(opts: NormalizedOpts) {
         // will generates builds for esm module
         // This make sure that typescript files are emitted at least once before we attempt to copy
         // its declaration files
-        inputs.length > 1 && index === 1 && options.format.includes('esm')
+        inputs.length > 1 && index === 1 && opts.format.includes('esm')
       );
       return configurations.rollup(config);
     })
